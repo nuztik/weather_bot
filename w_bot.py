2ff6745642f3fa0d80cda1e32960610f4c50386b
Svetlana <snuzdina2@gmail.com>
2023-08-22 16:01:02 +0300

    first commit

---
+++ w_bot.py
@@ -0,0 +1,57 @@
+import datetime
+import requests
+import json
+from pprint import pprint
+
+from config import API_W
+
+
+def weather(city, API_W):
+
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026C8",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+
+    }
+
+    try:
+        r = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_W}&units=metric&lang=ru")
+        data = r.json()
+        # pprint(data)
+
+        city = data['name']
+        temp = data['main']['temp']
+
+        weather_descrition = data['weather'][0]['main']
+        if weather_descrition in code_to_smile:
+            wd = code_to_smile[weather_descrition]
+        else:
+            wd = 'Посмотри в окно, я не могу определиться'
+
+        humidity = data['main']['humidity']
+        wind = data['wind']['speed']
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data['sys']['sunrise'])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data['sys']['sunset'])
+        print(f'*** {datetime.datetime.now().strftime("%Y-%m-%d_%H:%M")} ***\n'
+            f'Погода сейчас в городе: {city}\n Температура:{temp} С° {wd}\n'
+              f'Влажность:{humidity}\n Скорость ветра:{wind} м/с\n'
+              f'Восход солнца:{sunrise_timestamp}\n Закат солнца:{sunset_timestamp}\n')
+
+    except Exception as ex:
+        print(ex)
+        print('Такой город не найден, напишите другой.')
+
+
+
+def main():
+    city = input('Введите название города: ')
+    weather(city, API_W)
+
+
+if __name__ == '__main__':
+    main()
